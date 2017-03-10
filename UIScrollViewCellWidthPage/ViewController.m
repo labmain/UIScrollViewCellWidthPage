4520021409018ba98abdfc248ea855971a06b576
shun <wangshun8888@gmail.com>
2017-03-10 18:25:18 +0800

    Initial Commit

---
+++ UIScrollViewCellWidthPage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UIScrollViewCellWidthPage
+//
+//  Created by 王顺 on 2017/3/10.
+//  Copyright © 2017年 shun wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
